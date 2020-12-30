14e44d4d64d70c1a4982b649a7deac4332bb6d25
Your Name <you@example.com>
2020-12-30 16:12:06 -0500

    Leer los valores de un select.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,28 @@
 /*
     Variables
 */
-const resultado = document.querySelector("#resultado");
-const anio = document.querySelector("#year");
+const marca = document.querySelector("#marca");
+const minimo = document.querySelector("#minimo");
+const maximo = document.querySelector("#maximo");
+const puertas = document.querySelector("#puertas");
+const transmision = document.querySelector("#transmision");
+const color = document.querySelector("#color");
+const year = document.querySelector("#year");
+
 const max = new Date().getFullYear();
 const min = max - 10;
+
+const resultado = document.querySelector("#resultado");
+
+const datosBusqueda = {
+  marca: "",
+  year: "",
+  minimo: "",
+  maximo: "",
+  puertas: "",
+  transmision: "",
+  color: "",
+};
 
 /*
     Eventos
@@ -12,6 +30,35 @@ const min = max - 10;
 document.addEventListener("DOMContentLoaded", () => {
   mostrarAutos();
   llenarAnios();
+});
+
+marca.addEventListener("change", (e) => {
+  datosBusqueda.marca = e.target.value;
+});
+
+year.addEventListener("change", (e) => {
+  datosBusqueda.year = e.target.value;
+});
+
+minimo.addEventListener("change", (e) => {
+  datosBusqueda.minimo = e.target.value;
+});
+
+maximo.addEventListener("change", (e) => {
+  datosBusqueda.maximo = e.target.value;
+});
+
+puertas.addEventListener("change", (e) => {
+  datosBusqueda.puertas = e.target.value;
+});
+
+transmision.addEventListener("change", (e) => {
+  datosBusqueda.transmision = e.target.value;
+});
+
+color.addEventListener("change", (e) => {
+  datosBusqueda.color = e.target.value;
+  console.log(datosBusqueda);
 });
 
 /*
