611fb6b5fe02e95febdb9d40f909faf471123d22
Your Name <you@example.com>
2020-12-30 18:39:04 -0500

    Filtra y muestra los automoviles.

--- js/app.js
+++ js/app.js
@@ -28,44 +28,58 @@ const datosBusqueda = {
     Eventos
 */
 document.addEventListener("DOMContentLoaded", () => {
-  mostrarAutos();
+  mostrarAutos(autos);
   llenarAnios();
 });
 
 marca.addEventListener("change", (e) => {
   datosBusqueda.marca = e.target.value;
+  filtrarAutos();
 });
 
 year.addEventListener("change", (e) => {
   datosBusqueda.year = e.target.value;
+  filtrarAutos();
 });
 
 minimo.addEventListener("change", (e) => {
   datosBusqueda.minimo = e.target.value;
+  filtrarAutos();
 });
 
 maximo.addEventListener("change", (e) => {
   datosBusqueda.maximo = e.target.value;
+  filtrarAutos();
 });
 
 puertas.addEventListener("change", (e) => {
   datosBusqueda.puertas = e.target.value;
+  filtrarAutos();
 });
 
 transmision.addEventListener("change", (e) => {
   datosBusqueda.transmision = e.target.value;
+  filtrarAutos();
 });
 
 color.addEventListener("change", (e) => {
   datosBusqueda.color = e.target.value;
-  console.log(datosBusqueda);
+  filtrarAutos();
 });
 
 /*
     Funciones
 */
-function mostrarAutos() {
-  autos.forEach((auto) => {
+function limpiarHTML() {
+  //contendorCarrito.innerHTML = "";
+  while (resultado.firstChild) {
+    resultado.removeChild(resultado.firstChild);
+  }
+}
+
+function mostrarAutos(autosR) {
+  limpiarHTML();
+  autosR.forEach((auto) => {
     const autoHTML = document.createElement("p");
     autoHTML.innerHTML = `
             <p>${auto.marca} ${auto.modelo} - ${auto.year} - ${auto.puertas} Puertas - Transmisión: ${auto.transmision} - Precio: ${auto.precio} - Color: ${auto.color}</p>
@@ -83,4 +97,66 @@ function llenarAnios() {
     opcion.textContent = i;
     year.appendChild(opcion);
   }
+}
+
+function filtrarAutos() {
+  const resul = autos
+    .filter(filtrarMarca)
+    .filter(filtrarAnio)
+    .filter(filtrarPuertas)
+    .filter(filtrarTransmision)
+    .filter(filtrarColor)
+    .filter(filtrarMin)
+    .filter(filtrarMax);
+
+  mostrarAutos(resul);
+}
+
+function filtrarMarca(auto) {
+  if (datosBusqueda.marca) {
+    return auto.marca === datosBusqueda.marca;
+  }
+  return auto;
+}
+
+function filtrarAnio(auto) {
+  if (datosBusqueda.year) {
+    return auto.year === parseInt(datosBusqueda.year);
+  }
+  return auto;
+}
+
+function filtrarPuertas(auto) {
+  if (datosBusqueda.puertas) {
+    return auto.puertas === parseInt(datosBusqueda.puertas);
+  }
+  return auto;
+}
+
+function filtrarTransmision(auto) {
+  if (datosBusqueda.transmision) {
+    return auto.transmision === datosBusqueda.transmision;
+  }
+  return auto;
+}
+
+function filtrarColor(auto) {
+  if (datosBusqueda.color) {
+    return auto.color === datosBusqueda.color;
+  }
+  return auto;
+}
+
+function filtrarMin(auto) {
+  if (datosBusqueda.minimo) {
+    return auto.precio >= datosBusqueda.minimo;
+  }
+  return auto;
+}
+
+function filtrarMax(auto) {
+  if (datosBusqueda.maximo) {
+    return auto.precio <= datosBusqueda.maximo;
+  }
+  return auto;
 }
